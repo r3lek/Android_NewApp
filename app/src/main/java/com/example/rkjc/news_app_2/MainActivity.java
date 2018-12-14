f7bf2c6698cefce76aaed3cc1b7a5e65258d08f9
r3lek <AILD213@yahoo.com>
2018-12-14 10:04:15 -0800

    Working oush notif

--- app/src/main/java/com/example/rkjc/news_app_2/MainActivity.java
+++ app/src/main/java/com/example/rkjc/news_app_2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.rkjc.news_app_2;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Network;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
@@ -19,7 +22,7 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -48,8 +51,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent notificationIntent = new Intent(this, AlarmReceiver.class);
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 10);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast);
+
 
         mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
 
---
+++ app/src/main/java/com/example/rkjc/news_app_2/NotificationActivity.java
@@ -0,0 +1,18 @@
+package com.example.rkjc.news_app_2;
+
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class NotificationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_activity);
+    }
+}
